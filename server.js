1ae60f4997c283166fec65f708ff53f387b6a9b3
melliti <ramzi.melliti@epitech.eu>
2019-01-23 23:15:53 +0100

    [INIT] Simple node server with socketIO connection

---
+++ server.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res) {
+    fs.readFile('./index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"content-type": "text/html"});
+        res.end(content);
+    });
+});
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connect', function(socket) {
+    socket.broadcast.emit('message', 'Un utilisateur s\'est connecté');
+    socket.on('message', function(client) {
+        socket.broadcast.emit('client', client.pseudo + ' vous dit: ' + client.message);
+    })
+});
+
+server.listen(8000);
